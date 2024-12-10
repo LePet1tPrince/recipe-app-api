da6ea17263ddf7d9066448868a13b429cb476917
LePetitPrince <timmylbender@gmail.com>
2024-12-10 15:56:25 -0500

    Added superuser support - 51

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -39,3 +39,14 @@ class ModelTests(TestCase):
 
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user('', 'test123')
+
+
+    def test_create_superuser(self):
+        """Test creating a super user"""
+        user = get_user_model().objects.create_superuser(
+            'test1@example.com',
+            'test123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
