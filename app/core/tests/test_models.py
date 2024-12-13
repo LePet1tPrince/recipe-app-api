938968e4f148f54abd9ec8389ac7b3c19be69a97
LePetitPrince <timmylbender@gmail.com>
2024-12-13 16:50:12 -0500

    adding tag to admin

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -14,8 +14,6 @@ def create_user(email='user@example.com', password='testpass2134'):
     return get_user_model().objects.create_user(email, password)
 
 
-
-
 class ModelTests(TestCase):
     """Test models"""
 
